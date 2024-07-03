6a91da8c1652e39ea2994fd859625a52f7fce219
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-07-03 18:33:55 +0200

    Login finished

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -14,10 +14,33 @@ class Login extends BaseController
 
     public function doLogin() {
 
-        $model = new UserModel();
+        $userModel = new UserModel();
 
-        echo "Submitted";
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $result = $userModel->where('email', $email)->first();
+
+        if($result->id > 0) {
+            if(password_verify($password, $result->password)) {
+
+                $this->session->set('user_id', $result->id);
+                return redirect()->to('/dashboard');
+
+            } else {
+                echo 'Invalid user name or password';
+            }
+        } else {
+            echo 'Invalid user name or password';
+        }
 
 
     }
+
+    public function logout() {
+        $this->session->destroy();
+        return redirect()->to('/login');
+    }
+
+
 }
